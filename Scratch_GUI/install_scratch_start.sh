2bc7eb3f9798d1d2350090aff3f289881539afde
Dexter Industries <dxtrindustries@gmail.com>
2015-07-21 13:15:19 +0400

    Change install script

--- Scratch_GUI/install_scratch_start.sh
+++ Scratch_GUI/install_scratch_start.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 # Install the Scratch GUI.
 
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n
-sudo apt-get install python-psutil
+# sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n -y
+# sudo apt-get install python-psutil -y
 
 # Copy shortcut to desktop.
-
+cp Scratch_Start.desktop /home/pi/Desktop
 # Make shortcut executable
-
+sudo chmod +x /home/pi/Desktop/Scratch_Start.desktop
 # Make run_scratch_gui executable.
+sudo chmod +x /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.sh
