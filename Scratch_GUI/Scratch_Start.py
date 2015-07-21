7df57573e885d4fff39ec5116dcecf5fbc44bade
Dexter Industries <dxtrindustries@gmail.com>
2015-07-21 13:59:24 +0400

    Scratch Start with example broadcasting on.

--- Scratch_GUI/Scratch_Start.py
+++ Scratch_GUI/Scratch_Start.py
@@ -184,7 +184,7 @@ class MainPanel(wx.Panel):
 		write_debug("Programming Started.")	
 		
 		# Start Scratch
-		start_command = "scratch"
+		start_command = "scratch /home/pi/Desktop/DexterEd/new.sb"
 		send_bash_command_in_background(start_command)
 		'''
 		dlg = wx.MessageDialog(self, 'Starting Scratch Programming!', 'Update', wx.OK|wx.ICON_INFORMATION)
--- Scratch_GUI/install_scratch_start.sh
+++ Scratch_GUI/install_scratch_start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Install the Scratch GUI.
 
-# sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n -y
-# sudo apt-get install python-psutil -y
+sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n -y
+sudo apt-get install python-psutil -y
 
 # Copy shortcut to desktop.
 cp Scratch_Start.desktop /home/pi/Desktop
@@ -10,3 +10,7 @@ cp Scratch_Start.desktop /home/pi/Desktop
 sudo chmod +x /home/pi/Desktop/Scratch_Start.desktop
 # Make run_scratch_gui executable.
 sudo chmod +x /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.sh
+
+# Remove Scratch Shortcuts if they're there.
+sudo rm /home/pi/Desktop/BrickPi_Scratch_Start.desktop
+sudo rm /home/pi/Desktop/GoPiGo_Scratch_Start.desktop
