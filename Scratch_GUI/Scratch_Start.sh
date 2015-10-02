f5866fb26845e99fcf5a1ef65b0d760c37e04f03
Dexter Industries <dxtrindustries@gmail.com>
2015-10-02 16:29:06 +0400

    Remove Scratch GUI Software.

--- Scratch_GUI/Scratch_Start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# sudo lxterminal --command "sudo xhost +"
-screen -d -r local
-sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py
-#sudo lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
-#lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
